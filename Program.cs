2a769a514038be0527c926b81deb7510ce6e6172
Danijel-James Wynyard <danijel@repasscloud.com>
2024-09-06 23:16:32 +1000

    init commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using CliqMate.Admin.Components;
+
+namespace CliqMate.Admin;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
